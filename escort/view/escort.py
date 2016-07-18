688adfda723163f8678d8f97b0cbc8d38d04872e
Taylor <tank357@icloud.com>
2016-07-18 11:27:38 +0800

    更换wechat框架

--- escort/view/escort.py
+++ escort/view/escort.py
@@ -7,25 +7,47 @@ sys.path.append("..")
 from flask import Flask, request, render_template, redirect, url_for, make_response
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.base import expose
 
-from wechat_sdk.exceptions import ParseError
-
+from flask_httpauth import HTTPBasicAuth
 from model.base import db_session
 from model.order import Order
-from wechat import wechat
+import wechat
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 admin = Admin(app)
 admin.add_view(ModelView(Order, db_session))
+
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
+
+users = {
+    'taylor': 'happytimeadmin',
+}
+
 
 @app.before_request
 def pre():
     pass
 
 
+@auth.get_password
+def get_password(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+
+@app.route('/admin')
+@auth.login_required
+@expose
+def admin_view():
+    return admin.endpoint
+
+
 @app.route('/')
+@auth.login_required
 def hello_world():
     return 'Hello escort!'
 
@@ -57,7 +79,7 @@ def create_menu():
             }
         ]
     }
-    return str(wechat.create_menu(menu_data=menu))
+    # return str(wechat.create_menu(menu_data=menu))
 
 
 def subscribe(key, ticket):
@@ -90,39 +112,8 @@ def wexin():
         if echostr != '':
             return echostr
         else:
-            try:
-                body = request.data
-                wechat.parse_data(body)
-                if isinstance(wechat.message, wechat.EventMessage):
-                    if wechat.message.type == 'subscribe':  # 关注事件(包括普通关注事件和扫描二维码造成的关注事件)
-                        key = wechat.message.key  # 对应于 XML 中的 EventKey (普通关注事件时此值为 None)
-                        ticket = wechat.message.ticket  # 对应于 XML 中的 Ticket (普通关注事件时此值为 None)
-                        subscribe(key, ticket)
-                    elif wechat.message.type == 'unsubscribe':  # 取消关注事件（无可用私有信息）
-                        pass
-                    elif wechat.message.type == 'scan':  # 用户已关注时的二维码扫描事件
-                        key = wechat.message.key  # 对应于 XML 中的 EventKey
-                        ticket = wechat.message.ticket  # 对应于 XML 中的 Ticket
-                        scan(key, ticket)
-                    elif wechat.message.type == 'location':  # 上报地理位置事件
-                        latitude = wechat.message.latitude  # 对应于 XML 中的 Latitude
-                        longitude = wechat.message.longitude  # 对应于 XML 中的 Longitude
-                        precision = wechat.message.precision  # 对应于 XML 中的 Precision
-                        location(latitude, longitude, precision)
-                    elif wechat.message.type == 'click':  # 自定义菜单点击事件
-                        key = wechat.message.key  # 对应于 XML 中的 EventKey
-                        click(key)
-                    elif wechat.message.type == 'view':  # 自定义菜单跳转链接事件
-                        key = wechat.message.key  # 对应于 XML 中的 EventKey
-                        view(key)
-                    elif wechat.message.type == 'templatesendjobfinish':  # 模板消息事件
-                        status = wechat.message.status  # 对应于 XML 中的 Status
-                    elif wechat.message.type in ['scancode_push', 'scancode_waitmsg', 'pic_sysphoto',
-                                                 'pic_photo_or_album', 'pic_weixin', 'location_select']:  # 其他事件
-                        key = wechat.message.key  # 对应于 XML 中的 EventKey
-                        # code goes here
-            except ParseError:
-                print 'Invalid Body Text'
+            # code goes here
+            pass
     else:
         return 'you are not wechat,fuck off!'
 
--- escort/view/wechat.py
+++ escort/view/wechat.py
@@ -3,26 +3,10 @@ import functools
 import os
 
 import sys
-from flask import request, redirect
-from requests import session
-from wechat_sdk import WechatConf, WechatBasic
-from werkzeug.exceptions import abort
-
+from wechatpy.exceptions import InvalidSignatureException
+from wechatpy.utils import check_signature
 sys.path.append("..")
-first = True
 token = "flowersandgrassmakesunine"
 app_id = "wx74f11559acca4843"
 app_secret = "049c8dfaabc0ba862d11d6b1bba8c264"
 encoding_aes_key = "aI3oNaHcRncU2mIRsAfNb9sSz1KyfT2DVa3nSG9rxws"
-
-conf = WechatConf(
-    token=token,
-    appid=app_id,
-    appsecret=app_secret,
-    encrypt_mode='safe',
-    # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
-    encoding_aes_key=encoding_aes_key
-    # 如果传入此值则必须保证同时传入 token, appid
-)
-
-wechat = WechatBasic(conf=conf)
